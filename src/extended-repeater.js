88b3551def3fd50f6aeb6fbbbb2ba2ba790ba55d
EVGEN <k.e.l@tut.by>
2022-04-30 11:46:47 +0300

    complete task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,61 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (options.separator === undefined) {
+    options.separator = "+";
+  }
+
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = "|";
+  }
+
+  if (
+    options.additionRepeatTimes === undefined &&
+    options.repeatTimes === undefined
+  ) {
+    options.repeatTimes = 1;
+    options.additionRepeatTimes = 1;
+  }
+
+  str = String(str);
+  options.addition = String(options.addition);
+
+  let additionAndAdditionSeparator = "";
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    additionAndAdditionSeparator += options.addition;
+    if (i < options.additionRepeatTimes - 1) {
+      additionAndAdditionSeparator += options.additionSeparator;
+    }
+  }
+
+  let result = "";
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    result += str + additionAndAdditionSeparator;
+    if (i < options.repeatTimes - 1) {
+      result += options.separator;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
